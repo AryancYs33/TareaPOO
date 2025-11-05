704afa4e21021112d54bca5291000e4f5d63ab52
awwq8293-pixel <186656368+AryancYs33@users.noreply.github.com>
2025-11-05 02:05:03 -0500

    istema legado

---
+++ src/Libreríaantigua/LegacyBillingSystem.java
@@ -0,0 +1,8 @@
+class LegacyBillingSystem {
+    public void createInvoice(String customerName, double amount) {
+        System.out.println("[SISTEMA LEGADO] Generando factura...");
+        System.out.println("Cliente: " + customerName);
+        System.out.println("Monto: S/. " + amount);
+        System.out.println("Factura generada exitosamente");
+    }
+}
