dbe6b4e2c66c50c569a074202866cd0f703add52
awwq8293-pixel <186656368+AryancYs33@users.noreply.github.com>
2025-11-05 22:43:35 -0500

    primeras clases

--- src/Libreríaantigua/LegacyBillingSystem.java
+++
@@ -1,8 +0,0 @@
-class LegacyBillingSystem {
-    public void createInvoice(String customerName, double amount) {
-        System.out.println("[SISTEMA LEGADO] Generando factura...");
-        System.out.println("Cliente: " + customerName);
-        System.out.println("Monto: S/. " + amount);
-        System.out.println("Factura generada exitosamente");
-    }
-}
---
+++ src/adapter/FacturaAdapter.java
@@ -0,0 +1,15 @@
+package adapter;
+
+import legacy.LegacyBillingSystem;
+
+public class FacturaAdapter implements FacturaService {
+    private final LegacyBillingSystem legacy = new LegacyBillingSystem();
+    
+
+    @Override
+    public String generarFactura(String cliente, String producto, double total) {
+        String razonSocial = cliente;
+        String ruc = "00000000000";
+        return legacy.emitirFactura(razonSocial, ruc, total);
+    }
+}
