ab2042be3f1cd0c768d76ce6841fc6a1a73b7110
awwq8293-pixel <186656368+AryancYs33@users.noreply.github.com>
2025-11-05 23:23:08 -0500

    main prt2

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,3 +6,13 @@ import services.PedidoRepository;
 import services.StockService;
 import services.TaxService;
 import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        PedidoFacade facade = new PedidoFacade(
+                new StockService(),
+                new TaxService(),
+                new PedidoRepository(),
+                new FacturaAdapter()
+        ); 
