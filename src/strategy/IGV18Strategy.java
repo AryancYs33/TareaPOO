2cf4a6eea4040c0335aeb1ed4578db7b70dc2d6d
awwq8293-pixel <186656368+AryancYs33@users.noreply.github.com>
2025-11-09 17:59:21 -0500

    calculador pata el impuesto

--- src/strategy/IGV18Strategy.java
+++ src/strategy/IGV18Strategy.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package strategy;
 
-/**
- *
- * @author Personal
- */
-public class IGV18Strategy {
-    
-}
+public class IGV18Strategy implements ImpuestoStrategy {
+    @Override
+    public double calcular(double subtotal) {
+        return Math.round(subtotal * 0.18 * 100.0) / 100.0;
+    }
+} 
