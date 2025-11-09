b4583d224a6feee7f1db86e33befe33ce43992b1
awwq8293-pixel <186656368+AryancYs33@users.noreply.github.com>
2025-11-09 18:14:13 -0500

    cmbaindo main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,29 +4,48 @@ import adapter.FacturaAdapter;
 import facade.PedidoFacade;
 import services.PedidoRepository;
 import services.StockService;
-import services.TaxService;
-import java.util.Scanner;
+import strategy.ExoneradoStrategy;
+import strategy.IGV18Strategy;
+import strategy.ImpuestoStrategy;
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        PedidoFacade facade = new PedidoFacade(
-                new StockService(),
-                new TaxService(),
-                new PedidoRepository(),
-                new FacturaAdapter()
-        ); 
+        Scanner sc = new Scanner(System.in);
 
-Scanner sc = new Scanner(System.in);
-        System.out.println("=== Registro de Pedido (Facade + Adapter) ===");
+        System.out.println("Seleccione estrategia de impuesto:");
+System.out.println("1) IGV 18%");
+System.out.println("2) Exonerado (0%)");
+System.out.print("Opcion: ");
+String op = sc.nextLine();
+
+ImpuestoStrategy strategy;
+switch (op) {
+    case "2":
+        strategy = new ExoneradoStrategy();
+        break;
+    default:
+        strategy = new IGV18Strategy();
+        break;
+}
+
+PedidoFacade facade = new PedidoFacade(
+        new StockService(),
+        new PedidoRepository(),
+        new FacturaAdapter(),
+        strategy
+);
+
+
         System.out.print("Cliente: ");
         String cliente = sc.nextLine();
         System.out.print("Producto (laptop/mouse/teclado): ");
         String producto = sc.nextLine();
         System.out.print("Cantidad: ");
         int cantidad = Integer.parseInt(sc.nextLine());
-        
-try {
+
+        try {
             System.out.println(facade.procesarPedido(cliente, producto, cantidad));
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
