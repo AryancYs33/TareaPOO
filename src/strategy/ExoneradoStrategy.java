467c718f6693ed02ece7e6cc7aff05bfc7cb5feb
awwq8293-pixel <186656368+AryancYs33@users.noreply.github.com>
2025-11-09 17:49:42 -0500

    impuesto

--- src/strategy/ExoneradoStrategy.java
+++ src/strategy/ExoneradoStrategy.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package strategy;
 
-/**
- *
- * @author Personal
- */
-public class ExoneradoStrategy {
-    
+public class ExoneradoStrategy implements ImpuestoStrategy {
+    @Override
+    public double calcular(double subtotal) {
+        return 0.0;
+    }
 }
