a0389d7512057ece109acbf17f3e83bcfa3017e5
awwq8293-pixel <186656368+AryancYs33@users.noreply.github.com>
2025-11-09 17:47:11 -0500

    agregando nevas clases, creando paquete stratefy

--- src/adapter/FacturaAdapter.java
+++ src/adapter/FacturaAdapter.java
@@ -10,6 +10,7 @@ public class FacturaAdapter implements FacturaService {
     public String generarFactura(String cliente, String producto, double total) {
         String razonSocial = cliente;
         String ruc = "00000000000";
-        return legacy.emitirFactura(razonSocial, ruc, total);
+        String legacyFactura = legacy.emitirFactura(razonSocial, ruc, total);
+    return "Factura generada para " + cliente + " (RUC " + ruc + ") por S/ " + total;
     }
 }
